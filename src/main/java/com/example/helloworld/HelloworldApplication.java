8f7823af0b49e9e736c6173287703cb122444a78
PakkurthiChandu <chandupakkurthi@gmail.com>
2026-04-30 18:02:44 +0530

    clean code

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
     public static void main(String[] args) {
-        // This will only crash when running inside a Kubernetes Cluster
-        if (System.getenv("KUBERNETES_SERVICE_HOST") != null) {
-            System.err.println("FATAL: Kubernetes environment detected. Crashing for testing purposes...");
-            System.exit(1);
-        }
-
         SpringApplication.run(HelloworldApplication.class, args);
     }
 

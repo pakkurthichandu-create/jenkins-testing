227f07617329939ff45e8d01349f8c779c785144
PakkurthiChandu <chandupakkurthi@gmail.com>
2026-04-30 18:26:18 +0530

    addrd crash code

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
     public static void main(String[] args) {
+        // SAFETY TEST: This app will crash if running in Kubernetes
+        if (System.getenv("KUBERNETES_SERVICE_HOST") != null) {
+            System.err.println("TESTING SAFETY: Kubernetes detected. Crashing to see if the old Pod stays alive...");
+            System.exit(1);
+        }
+        
         SpringApplication.run(HelloworldApplication.class, args);
     }
 

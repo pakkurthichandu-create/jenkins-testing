dc7b9d63d4079ca3ad6ec206c6ba2d8155e32c44
PakkurthiChandu <chandupakkurthi@gmail.com>
2026-04-30 18:29:55 +0530

    crash

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
     public static void main(String[] args) {
-        // SAFETY TEST: This app will crash if running in Kubernetes
-        if (System.getenv("KUBERNETES_SERVICE_HOST") != null) {
-            System.err.println("TESTING SAFETY: Kubernetes detected. Crashing to see if the old Pod stays alive...");
-            System.exit(1);
-        }
-        
+        int crash = 1 / 0;
         SpringApplication.run(HelloworldApplication.class, args);
     }
 

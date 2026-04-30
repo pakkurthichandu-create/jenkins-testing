c45356cdfb8d55e10161a2cd7cf65e5c0296091e
PakkurthiChandu <chandupakkurthi@gmail.com>
2026-04-30 17:56:09 +0530

    change the code

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -2,32 +2,12 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
-import jakarta.annotation.PostConstruct;
-import java.net.InetAddress;
 
 @SpringBootApplication
 public class HelloworldApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HelloworldApplication.class, args);
-    }
-}
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
 
-@Component
-class HealthChecker {
-
-    @PostConstruct
-    public void init() {
-        try {
-            // DEVOPS CHALLENGE: This app depends on a service named 'payment-service'
-            // It will check if 'payment-service' is reachable on the network.
-            // If it can't find it, it crashes.
-            InetAddress address = InetAddress.getByName("payment-service");
-            System.out.println("NETWORK SUCCESS: Connected to " + address.getHostAddress());
-        } catch (Exception e) {
-            System.err.println("NETWORK FATAL ERROR: Cannot find 'payment-service'. Is the K8s Service created?");
-            System.exit(1);
-        }
-    }
 }

ae4732e0d87411cc75d643b2636faad523a4b05d
PakkurthiChandu <chandupakkurthi@gmail.com>
2026-04-30 18:48:43 +0530

    crash

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
     public static void main(String[] args) {
+        int crash = 1 / 0;
         SpringApplication.run(HelloworldApplication.class, args);
     }
 

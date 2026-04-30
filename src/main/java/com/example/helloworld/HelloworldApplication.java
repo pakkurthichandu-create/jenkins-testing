807055ea46b0ebde3257bf14d2a2917183a0e090
PakkurthiChandu <chandupakkurthi@gmail.com>
2026-04-30 17:32:47 +0530

    added crash code

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,6 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
+		// INTENTIONAL ERROR: This will cause a CrashLoopBackOff in Kubernetes
+		String appMode = System.getenv("APP_MODE");
+		if (!"PROD".equals(appMode)) {
+			System.err.println("FATAL ERROR: Environment variable 'APP_MODE' is missing or incorrect.");
+			System.err.println("The application will now shut down to simulate a CrashLoopBackOff.");
+			System.exit(1);
+		}
+		
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
 

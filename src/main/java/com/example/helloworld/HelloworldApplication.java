77105e617520a580fc747277a59eb3c8eb1e6764
PakkurthiChandu <chandupakkurthi@gmail.com>
2026-05-01 17:13:28 +0530

    clean

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -8,16 +8,7 @@ public class HelloworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
-        
-        System.out.println("Starting memory intensive operation...");
-        // This will allocate a lot of memory to trigger an OOMKilled state
-        java.util.List<byte[]> memoryEater = new java.util.ArrayList<>();
-        while (true) {
-            memoryEater.add(new byte[1024 * 1024]); // Add 1MB at a time
-            if (memoryEater.size() % 10 == 0) {
-                System.out.println("Allocated: " + memoryEater.size() + "MB");
-            }
-        }
+        System.out.println("Application is CLEAN, STABLE, and HEALTHY!");
     }
 
 }
